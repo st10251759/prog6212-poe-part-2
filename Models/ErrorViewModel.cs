f0a50e01ebc5243493942c9946235d6e854114b1
Cameron Chetty <ST10251759@vcconnect.edu.za>
2024-10-11 06:19:21 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10251759_PROG6212_POE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
