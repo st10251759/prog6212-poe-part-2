920a21868fb00cb40cbb8993ce34691c75d1f24e
Cameron Chetty <ST10251759@vcconnect.edu.za>
2024-10-11 19:52:09 +0200

    Updated Links to display based on the role of user logged in, different links will display if user is a Lecturer, Programme Coordinator or Academic Manager

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace ST10251759_PROG6212_POE.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
